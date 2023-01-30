ab22c19e61b4c3e305868ec9f6e1dcb634cef2a8
Konrad <chrzanekk@gmail.com>
2023-01-30 19:41:16 +0100

    added frontend project files and start to implement frontend features

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) { }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      
+    )
+    
+}
+}
